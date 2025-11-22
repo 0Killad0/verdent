914ee623e589d552ed8b58039087ea17a9ee7ea2
Abhijith PN <abhijithnaveen790@gmail.com>
2025-11-22 12:09:37 +0530

    avumayirikum

--- frontend/src/pages/checkout/PlaceOrder.jsx
+++ frontend/src/pages/checkout/PlaceOrder.jsx
@@ -12,7 +12,7 @@ const PlaceOrder = () => {
   const location = useLocation();
   const [loading, setLoading] = useState(false);
 
-  const API_BASE_URL = "http://localhost:5000";
+  const API_BASE_URL = "https://verdent-production.up.railway.app";
 
   const { selectedAddress, shippingMethod, shippingCost, paymentMethod } = location.state || {};
 
